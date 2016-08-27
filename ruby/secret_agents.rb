fbf20a3025a8ce585c96921ef584eb5ccb5f16b6
grandtheft-spacehip <justdizon71@gmail.com>
2016-08-26 17:40:48 -0700

    Created Encryption/Decryption methods with Jules

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,67 @@
+# Release 2: Build an Encryption Program
+
+=begin
+	
+rescue Exception => e
+	
+end
+# Pseudocode for "Encrypt"
+
+# 1. Input Password
+# 2. Get password length
+# 3. Loop through each letter within the string
+# 4. Advance each letter within string to the next letter in the Alphabet
+# 5. Return the new string
+
+
+password = "abc"
+
+index = 0
+
+while index < password.length
+	password[index] = password[index].next
+
+	index += 1
+end
+
+puts password
+
+=end
+
+# Pseudocode for "Decrypt"
+
+# 1. Input Password
+# 2. Get password length
+# 3. Loop through each letter with the string
+# 4. For each letter within the string, find the index according to the Alphabet
+# 5. Change each letter within the string to the previous letter in the Alphabet
+# 6. Return the new string
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+password = "bcd"
+
+index = 0
+
+while index < password.length
+	each_letter = password[index] # Gets each letter within 'password' variable and stores within 'each_letter' variable individually
+	letter_index = alphabet.index(each_letter) # Gets the index of 'each_letter' according to the Alphabet and stores within 'letter_index' variable
+	new_letter = alphabet[letter_index - 1] # Subtracts 'letter_index' - 1, which means it will go to the previous letter according to the Alphabet and store the new letter within 'new_letter' variable
+	password[index] = new_letter # Get the current letter, 'password[index] and store 'new_letter' into that space
+	
+
+	index += 1
+end
+
+puts password
+
+
+
+
+
+
+
+
+
+
+
