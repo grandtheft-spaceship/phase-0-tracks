105052833d130cbcdb0ccbda312678d0f872b402
grandtheft-spacehip <justdizon71@gmail.com>
2016-08-31 19:34:24 -0700

    Created fibonacci method with Sierra

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+=begin
 def simple_search(array, integer)
 	index = 0
 
@@ -17,6 +18,22 @@ array = [1, 3, 4, 5]
 
 simple_search(array, 2)
 simple_search(array, 4)
+=end
 
+def fib(num)
+  fib_array = [0, 1]
 
+  if num == 0
+    p []
+  elsif num == 1
+    p [0]
+  else 
+    until fib_array.length == num
+      next_num = fib_array[-1] + fib_array[-2]
+      fib_array << next_num 
+    end
+  p fib_array
+  end
+end
 
+fib(100) 
