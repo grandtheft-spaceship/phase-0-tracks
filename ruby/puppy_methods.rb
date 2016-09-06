e52d4c367da457669cc62f44fd9c0ae53e0763bc
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-06 08:17:06 -0700

    Created class methods for puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    integer.times do
+      puts "Woof"
+    end
+  end
+
+  def roll_over
+    puts "*Rolls over"
+  end
+
+  def dog_years(integer)
+    dog_years = integer * 7
+
+    puts "Puppy is #{dog_years} years old."
+  end
+
+  def sit
+    puts "*Sits down"
+  end
 end
 
+Spike = Puppy.new
+Spike.fetch("ball")
+Spike.speak(3)
+Spike.roll_over
+Spike.dog_years(5)
+Spike.sit
