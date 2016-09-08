9ee85a71688e1ee330545eec9cfb0e2530bb19f8
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-08 09:51:48 -0700

    WIP // Not done

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,22 +17,49 @@ class Santa
     puts "That was the best #{cookie_flavor} cookie I've ever eaten!"
   end
 
+  def celebrate_birthday
+    @new_age = @age + 1
+  end
+
+  def get_mad_at(name)
+    puts "You messed up big time #{name}!!!"
+
+    @reinder_ranking.rotate!
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
 end
 
 gender = ["Agender", "Female", "Bigender", "Male", "Female", "Gender-Fluid"]
-
 ethnicity = ["Polynesian", "Asian", "African", "European", "Latino", "Middle Eastern"]
-
 orientation = ["Lesbian", "Gay", "Straight", "Bi", "Transgender", "Queer"]
-
 religion = ["Buddhism", "Isalm", "Christianity", "Judaism", "Taoism", "Sikhism"]
+
 
 all_santas = []
 
-gender.length.times do |which_santa|
-  all_santas << Santa.new(gender[which_santa], ethnicity[which_santa], orientation[which_santa], religion[which_santa])
+gender.length.times do |santa|
+  all_santas << Santa.new(gender[santa], ethnicity[santa], orientation[santa], religion[santa])
 end
 
-p all_santas
+puts "All Santas have been created! Plesae verify correct information:"
+
+all_santas.each_with_index do |santa_num, index|
+  puts "Santa Number: #{index + 1}"
+  puts "Age: #{santa_num.age}"
+  puts "Ethnicity: #{santa_num.ethnicity}"
+end
+
 
 
