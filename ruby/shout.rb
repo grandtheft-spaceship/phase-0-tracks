b1f2eadedc492feab764f200884daed3e827fa02
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-09 13:25:17 -0700

    Refactored module to become a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,37 @@
 module Shout
+  def yell_angrily(words)
+    p words + "!!!" + " >:["
+  end
+
+  def yell_happily(words)
+    p "¡" + words + "!"
+  end 
+end
+
+class Student
+  include Shout
+end
+
+class Teacher
+  include Shout
+end
+
+teacher = Teacher.new
+student = Student.new
+
+teacher_words = "You're going to the principal's office"
+student_words = "Yay! I get to go home early"
+
+teacher.yell_angrily(teacher_words)
+student.yell_happily(student_words)
+
+
+
+
+
+
+=begin
+module Shout
   def self.yell_angrily(words)
     words + "!!!" + " >:["
   end
@@ -10,3 +43,4 @@ end
 
 p Shout.yell_angrily("You're a numbnut")
 p Shout.yelling_happily("Happy Birthday")
+=end
