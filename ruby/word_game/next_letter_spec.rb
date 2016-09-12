ccdeefee6a4f2ad9a224d12973a3a8f71c557324
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-12 09:44:30 -0700

    Added driver code for user interface

--- ruby/word_game/next_letter_spec.rb
+++ ruby/word_game/next_letter_spec.rb
@@ -2,6 +2,7 @@ require_relative "next_letter"
 
 describe NextLetter do
   let(:game) { NextLetter.new }
+  let(:board) { NextLetter.new.setup_board("dog")}
 
   it "accepts mystery word and splits letters into an array" do
     expect(game.split("dog")).to eq ["d", "o", "g"]
@@ -11,12 +12,13 @@ describe NextLetter do
     expect(game.setup_board("dog")).to eq ["_","_","_"]
   end
 
-  it "will displau the current state of the board" do
-    expect(game.display_board).to eq ["_","_","_"]
+  it "will display the current state of the board" do
+    expect(board.display_board).to eq ["_","_","_"]
   end
 
   it "will verify the user's guess against the board" do
-    expect(game.verify("d")).to eq ["d","_","_"]
+    expect(board.verify_guess("d")).to eq ["d","_","_"]
   end
 
 end
+
