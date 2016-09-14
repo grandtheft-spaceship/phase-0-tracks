9e6f83ad8749ce6d499da94fed47d3a8ba77199f
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-13 19:50:39 -0700

    Wrote pseudocode for reverse method

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Method: String Reverse
+// 1. Define an empty string
+// 2. Define reverse method that takes in 1 parameter
+// 2. Define an empty string
+// 3. Split the string into its individual characters and their corresponding indices
+// 4. Starting with the largest index, push the corresponding character into the empty string
+// 5. Return the reversed-string 
+
+
+
