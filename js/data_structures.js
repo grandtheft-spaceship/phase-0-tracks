138f596de492275fcfe8c24cebd05d480a1917bd
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-16 14:16:22 -0700

    Release 1. Iterates through both arrays and storing key-value pairs into an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,9 +1,17 @@
 var colors = ["blue", "red", "green", "black"];
 
-var names = ["Penny", "Daisy", "Harry", "Potter"]
+var names = ["Penny", "Daisy", "Harry", "Potter"];
 
 colors.push("brown");
 names.push("George");
 
 console.log(colors);
 console.log(names);
+
+var horses = {};
+
+for (var x = 0; x < names.length; x++) {
+  horses[names[x]] = colors[x];
+}
+
+console.log(horses);
