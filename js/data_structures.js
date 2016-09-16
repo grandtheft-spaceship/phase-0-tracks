7752493618425534cc540dbb1e8e0b6caa0d82b9
grandtheft-spacehip <justdizon71@gmail.com>
2016-09-16 13:58:51 -0700

    Created two arrays and used .push method to add an item to each. Printed out arrays to test

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "red", "green", "black"];
+
+var names = ["Penny", "Daisy", "Harry", "Potter"]
+
+colors.push("brown");
+names.push("George");
+
+console.log(colors);
+console.log(names);
