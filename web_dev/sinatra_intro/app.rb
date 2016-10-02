b600c259d804aae356dcba30383e275b56444fd5
grandtheft-spacehip <justdizon71@gmail.com>
2016-10-02 00:16:48 -0700

    Created routes to test different ways of accessing/rendering data to the browser page

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -9,6 +9,7 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
+  p params
   "#{params[:name]} is #{params[:age]} years old."
 end
 
@@ -44,3 +45,42 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that retrieves
+# an address
+
+get "/students/:id/contact" do
+  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  "#{student['name']} lives at 333 Sesame St, Smallvile, KC"   
+end
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+get "/great_job" do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+
+get "/:number1/plus/:number2" do
+  number1 = params[:number1].to_i
+  number2 = params[:number2].to_i
+  added_numbers = number1 + number2
+
+  "#{number1} + #{number2} equals #{added_numbers}"
+end
+
+
+
+
+
+
+
+
+
+
